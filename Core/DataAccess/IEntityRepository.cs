8b2c721e05173008e187c33bcb239bf4f6c6a2f7
yakuprgn <93837191+yakuprgn@users.noreply.github.com>
2022-06-04 02:11:26 +0300

    Initial commit

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+//Core katmanı diğer katmanları referans almaz
+
+namespace Core.DataAccess
+{
+    //generic constraint
+    //classs : referans tip 
+    //IEntity : IEntity olabilir veya IEntitiy implemente eden bir nesne olabilir.
+    //new() : new'lenebilir olmalı.
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        //List<T> GetAllByCategory(int categoryId); Expression kullandığımız için buna ihtiyaç kalmıyor
+    }
+}
---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    //IEntity = Bu bir veritabanı nesnesidir
+    //IEntity implement eden class bir veritabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
